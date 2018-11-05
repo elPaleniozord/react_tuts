d1f9dd2844cdfc8c4a815d8fa1b2a25f5195fdb4
Jacek Walasik <jacekwalasik89@gmail.com>
2018-11-05 22:56:28 +0100

    lesson 17 manual data binding

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -23,13 +23,32 @@ const template = (
 );
 
 let count = 0;
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button id="my-id" className="button">+1</button>
-    </div>
-)
-console.log(templateTwo)
+const addOne = () => {
+    count++
+    renderCounterApp();
+}
+const minusOne = () => {
+    count-- 
+    renderCounterApp();
+}
+const resetCount = () => {
+    count=0
+    renderCounterApp();
+}
+
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp=() => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={resetCount}>Reset</button>
+        </div>
+    )
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
