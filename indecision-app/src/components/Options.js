60653da6ba1e68db9f38fc3c551f9626a9169fee
Jacek Walasik <jacekwalasik89@gmail.com>
2018-11-17 18:20:55 +0100

    lesson 55 complete

---
+++ indecision-app/src/components/Options.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Option from './Option.js'
+
+const Options = (props) => {
+    return(
+        <div>
+            {props.options.map((option)=>
+                (<Option 
+                    key={option} 
+                    optionText={option}
+                    handleDeleteOption={props.handleDeleteOption}
+                />)
+            )}
+            <button onClick={props.handleDeleteOptions}>Remove all</button>
+            {props.options.length === 0 && <p>Please add option to get started</p>}
+        </div>
+        
+    ) 
+}
+
+export default Options;
