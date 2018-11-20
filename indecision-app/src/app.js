513bf3ce852dae414b86d60ddcf8a2eebc48fc24
Jacek Walasik <jacekwalasik89@gmail.com>
2018-11-20 22:39:30 +0100

    lesson 64 complete

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp'
+import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp options={['Devils den', 'Second District']} />, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
