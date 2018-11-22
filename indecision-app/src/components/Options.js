812c44179c539261b37a2c8c367552ac4f07f957
Jacek Walasik <jacekwalasik89@gmail.com>
2018-11-22 22:42:11 +0100

    part one completed
    lesson 74 done

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import Option from './Option.js'
+import Option from './Option';
 
 const Options = (props) => (
-       <div>
-            {props.options.map((option)=>
-                (<Option 
-                    key={option} 
-                    optionText={option}
-                    handleDeleteOption={props.handleDeleteOption}
-                />)
-            )}
-            <button onClick={props.handleDeleteOptions}>Remove all</button>
-            {props.options.length === 0 && <p>Please add option to get started</p>}
-        </div>
-        
-)
+  <div>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+    </button>
+    </div>
+
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+    {
+      props.options.map((option, index) => (
+        <Option
+          key={option}
+          optionText={option}
+          count={index + 1}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
+);
 
 export default Options;
