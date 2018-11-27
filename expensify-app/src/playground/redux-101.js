a4f2c77042ff29e2029658374ebc570cc8bb2050
Jacek Walasik <jacekwalasik89@gmail.com>
2018-11-27 22:40:15 +0100

    lesson 87 complete

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,38 @@
-import {createStore} from 'redux';
+import {createStore, bindActionCreators} from 'redux';
 
-const store = createStore((state = {count: 0})=>{
-    return state;
+const store = createStore((state = {count: 0}, action)=>{
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy:1;
+            return {count: state.count+incrementBy}
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy:1;
+            return {count: state.count-decrementBy}
+        case 'RESET':
+            return {count: 0}
+        default: 
+            return state;
+    }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
+
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+})
+store.dispatch({
+    type: 'INCREMENT'
+})
+
+store.dispatch({
+    type: 'RESET'
+})
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+})
