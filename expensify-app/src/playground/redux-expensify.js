f1073aac293a342c64d7519679fc402890462135
Jacek Walasik <jacekwalasik89@gmail.com>
2018-12-02 22:36:44 +0100

    multiple reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,44 @@
 import { createStore, combineReducers } from 'redux';
 
+//add expense
+//remove expense
+//edit expense
+//set text filter
+//sort by date
+//sort by amnt
+//set start date
+//set end date
+
+const expensesReducerDefaultState =[];
+
+const expensesReducer = (state= expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+}
+//Filter reducer
+const filterReducerDefaultState = {
+  text:'',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+}
+
+const filterReducer = (state=filterReducerDefaultState, action) => {
+  switch(action.type) {
+    default: return state;
+  }
+}
+//store creation 
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filterReducer
+  })
+)
+
+console.log(store.getState())
+
 const demoState = {
   expenses: [{
     id: 'poijasdfhwer',
