be7c1682d6d88c1eca90baa156972c45dca7671f
Jacek Walasik <jacekwalasik89@gmail.com>
2018-12-15 22:38:29 +0100

    128 done

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -5,18 +5,18 @@ import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.addExpense(expense)
-    this.props.history.push('/')
-  }
+    this.props.addExpense(expense);
+    this.props.history.push('/');
+  };
   render() {
     return (
       <div>
         <h1>Add Expense</h1>
-        <ExpenseForm 
+        <ExpenseForm
           onSubmit={this.onSubmit}
         />
       </div>
-    ) 
+    );
   }
 }
 
