2d53183f4bcf5bbc97a852a5111bd27cf35469b3
Jacek Walasik <jacekwalasik89@gmail.com>
2018-12-15 18:35:23 +0100

    123 spies done

--- expensify-app/src/tests/components/ExpenseDashboardPage.test.js
+++ expensify-app/src/tests/components/ExpenseDashboardPage.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
-import {shallow} from 'enzyme';
 
-test("should render ExpenseDashboardPage", ()=>{
-    const wrapper = shallow(<ExpenseDashboardPage />)
-    expect(wrapper).toMatchSnapshot()
-})
+test('should render ExpenseDashboardPage correctly', () => {
+  const wrapper = shallow(<ExpenseDashboardPage />);
+  expect(wrapper).toMatchSnapshot();
+});
--- expensify-app/src/tests/components/ExpenseListItem.test.js
+++ expensify-app/src/tests/components/ExpenseListItem.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ExpenseListItem from '../../components/ExpenseListItem';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import expenses from '../fixtures/expenses';
+import ExpenseListItem from '../../components/ExpenseListItem';
 
-test('should render ExpenseListItem with expense 1',  ()=>{
-    const wrapper = shallow(<ExpenseListItem {...expenses[1]}/>)
-    expect(wrapper).toMatchSnapshot()
-})
+test('should render ExpenseListItem correctly', () => {
+  const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
