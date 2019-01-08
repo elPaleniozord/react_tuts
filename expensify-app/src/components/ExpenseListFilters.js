db26dc565d2c887948ba94f8f07417eb6dc554a8
Jacek Walasik <jacekwalasik89@gmail.com>
2019-01-08 22:55:02 +0100

    expensify complete

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -26,20 +26,28 @@ export class ExpenseListFilters extends React.Component {
   };
   render() {
     return (
-      <div>
-        <input
+      <div className="content-container">
+      <div className="input-group">
+        <div className="input-group__item">
+          <input
           type="text"
+          className="text-input"
+          placeholder="Search Expenses"
           value={this.props.filters.text}
           onChange={this.onTextChange}
         />
-        <select
+        </div>
+        <div className="input-group__item">
+          <select
+          className="select"
           value={this.props.filters.sortBy}
           onChange={this.onSortChange}
-        >
+          >
           <option value="date">Date</option>
           <option value="amount">Amount</option>
-        </select>
-        <DateRangePicker
+        </select></div>
+        <div className="input-group__item">
+          <DateRangePicker
           startDate={this.props.filters.startDate}
           endDate={this.props.filters.endDate}
           onDatesChange={this.onDatesChange}
@@ -48,7 +56,8 @@ export class ExpenseListFilters extends React.Component {
           showClearDates={true}
           numberOfMonths={1}
           isOutsideRange={() => false}
-        />
+        /></div>
+      </div>
       </div>
     );
   }
